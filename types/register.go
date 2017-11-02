04cb09ef6ced04d47837b29d654bc649abec471b
Parham Alvani <parham.alvani@gmail.com>
2017-11-02 12:09:02 +0330

    AUT ICPC registration model was added.

---
+++ types/register.go
@@ -0,0 +1,19 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 02-11-2017
+ * |
+ * | File Name:     types/register.go
+ * +===============================================
+ */
+
+package imex
+
+// Register of AUT-ICPC
+type Register struct {
+	Name      string
+	Institute string
+	Site      string
+	Status    string
+}
