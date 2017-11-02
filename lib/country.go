d8f1e86ec042bc03df54072bfd6d277bd7eb6f95
Parham Alvani <parham.alvani@gmail.com>
2017-11-02 18:25:20 +0330

    Creates lib folder for imex package.

---
+++ lib/country.go
@@ -0,0 +1,19 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 02-11-2017
+ * |
+ * | File Name:     country/country.go
+ * +===============================================
+ */
+
+package imex
+
+// CountryCodes consists of 3 character code of countries around the world
+var CountryCodes = map[string]string{
+	"Iran":                     "IRN",
+	"Germany":                  "DEU",
+	"Sweden":                   "SWE",
+	"United States of America": "USA",
+}
