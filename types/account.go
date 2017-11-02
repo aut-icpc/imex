c1fb5b0a6323fe2be309d4656d8150384540712b
Parham Alvani <parham.alvani@gmail.com>
2017-11-02 11:24:48 +0330

    Adds DomJudge structures.

---
+++ types/account.go
@@ -0,0 +1,19 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 02-11-2017
+ * |
+ * | File Name:     account.go
+ * +===============================================
+ */
+
+package impex
+
+// Account of domjudge
+type Account struct {
+	Type     string
+	FullName string
+	Username string
+	Password string
+}
---
+++ types/team.go
@@ -0,0 +1,22 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 02-11-2017
+ * |
+ * | File Name:     types/team.go
+ * +===============================================
+ */
+
+package impex
+
+// Team of domjudge
+type Team struct {
+	Number          int
+	EId             int
+	GId             int
+	Name            string
+	Institution     string
+	InstitutionCode string
+	CountryCode     string
+}
