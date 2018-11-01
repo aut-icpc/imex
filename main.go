53b252d497d586617172b6e3c39b1d7b92f773f8
Parham Alvani <parham.alvani@gmail.com>
2018-11-01 19:00:55 +0330

    Minor improvement in main.go

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/aut-icpc/imex/lib"
@@ -22,7 +23,7 @@ import (
 )
 
 func main() {
-	var rs map[string]map[int]imex.Register
+	var rs map[string]map[string]imex.Register
 	var fn string
 
 	fmt.Printf("AUT-ICPC JSON Filename: ")
@@ -32,9 +33,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	json.NewDecoder(f).Decode(&rs)
+	if err := json.NewDecoder(f).Decode(&rs); err != nil {
+		panic(err)
+	}
+	fmt.Println(rs)
 
-	// Onsite
+	// Onsite Teams
+	// generates password and account number for each team.
+	// final results are written to files.
 	fts, err := os.Create("teams-onsite.tsv")
 	if err != nil {
 		panic(err)
@@ -49,7 +55,8 @@ func main() {
 	}
 	fts.WriteString("teams\t1\n")
 	fas.WriteString("accounts\t1\n")
-	for id, r := range rs["onsite"] {
+	for strID, r := range rs["onsite"] {
+		id, _ := strconv.Atoi(strID)
 		t := imex.Team{
 			Number:       id + 1,
 			EId:          id + 100,
@@ -59,6 +66,7 @@ func main() {
 			CountryCode:  imex.CountryCodes[r.Site],
 			PrimaryEmail: r.Members["first"].Email,
 		}
+		fmt.Printf("%+v\n", t)
 		fts.WriteString(fmt.Sprintf("%d\t%d\t%d\t%s\t%s\t%s\t%s\n", t.Number, t.EId, t.GId, t.Name, t.Institution, t.InstitutionCode, t.CountryCode))
 		a := imex.Account{
 			Type:     "team",
@@ -66,11 +74,15 @@ func main() {
 			Username: fmt.Sprintf("%03d", t.Number),
 			Password: fmt.Sprintf("p%d", rand.Intn(100)),
 		}
+		fmt.Printf("%+v\n", a)
 		fas.WriteString(fmt.Sprintf("%s\t%s\t%s\t%s\n", a.Type, a.FullName, a.Username, a.Password))
 		fus.WriteString(fmt.Sprintf("%s, %s, %s\n", a.FullName, a.Username, a.Password))
 	}
+	return
 
-	// Online
+	// Online Teams
+	// generates password and account number for each team.
+	// final results are written to files and sent to their emails.
 	fto, err := os.Create("teams-online.tsv")
 	if err != nil {
 		panic(err)
@@ -81,7 +93,8 @@ func main() {
 	}
 	fto.WriteString("teams\t1\n")
 	fao.WriteString("accounts\t1\n")
-	for id, r := range rs["online"] {
+	for strID, r := range rs["online"] {
+		id, _ := strconv.Atoi(strID)
 		t := imex.Team{
 			Number:       id + 1,
 			EId:          id + 100,
@@ -91,6 +104,7 @@ func main() {
 			CountryCode:  imex.CountryCodes[strings.Title(r.Site)],
 			PrimaryEmail: r.Members["first"].Email,
 		}
+		fmt.Printf("%+v\n", t)
 		fto.WriteString(fmt.Sprintf("%d\t%d\t%d\t%s\t%s\t%s\t%s\n", t.Number, t.EId, t.GId, t.Name, t.Institution, t.InstitutionCode, t.CountryCode))
 		a := imex.Account{
 			Type:     "team",
@@ -98,6 +112,7 @@ func main() {
 			Username: fmt.Sprintf("%03d", t.Number),
 			Password: fmt.Sprintf("P%03d%03d", rand.Intn(1000), rand.Intn(1000)),
 		}
+		fmt.Printf("%+v\n", a)
 		fao.WriteString(fmt.Sprintf("%s\t%s\t%s\t%s\n", a.Type, a.FullName, a.Username, a.Password))
 		mail.SendMail(a.FullName, a.Username, a.Password, t.PrimaryEmail)
 	}
