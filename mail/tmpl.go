7a5546cffbc9797524bb8034e9eaa50f1d3983c6
Parham Alvani <parham.alvani@gmail.com>
2018-11-02 18:34:35 +0330

    Changes mail to online user-pass.

--- mail/tmpl.go
+++ mail/tmpl.go
@@ -19,13 +19,12 @@ var tmplString = `
         <body>
                 <h1>ACM ICPC AUT 18th</h1>
                 <p>Dear {{.Name}} of the team {{.Team}}</p>
-                <p>
-		Today's contest is scheduled to start at 2:30 p.m. Your team's username, password,
-		and any further information will be sent to you 30 minutes before the contest.
-		</p>
-
 		<p>
 		The contest is available <a href="http://daavar.ceit-ssc.ir/domjudge/login">here</a>.
+		</p>
+		<p>
+		Username: {{.Account.Username}}<br>
+		Password: {{.Account.Password}}
 		</p>
         </body>
 </html>
