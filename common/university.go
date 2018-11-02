e74a1d22e35c930ea1938a4b7a71088a16082e84
Parham Alvani <parham.alvani@gmail.com>
2018-11-02 16:24:10 +0330

    Adds university codes.

---
+++ common/university.go
@@ -0,0 +1,8 @@
+package common
+
+// InstitutionCodes contains a map between 3 character code of universities in the contest and their name.
+// AUT-ICPC website works with institution name so we must have a way to convert them.
+var InstitutionCodes = map[string]string{
+	"Amirkabir University of Technology": "AUT",
+	"Sharif University of Technology":    "SUT",
+}
