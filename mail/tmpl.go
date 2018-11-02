24d613c6ec3b32547c5e2c4c0d4528e9eda087d8
Parham Alvani <parham.alvani@gmail.com>
2018-11-02 12:41:08 +0330

    Completes hello template.

--- mail/tmpl.go
+++ mail/tmpl.go
@@ -13,20 +13,20 @@
 
 package mail
 
+// before contest email. this mail contains just general information about online contest.
 var tmplString = `
 <html>
         <body>
                 <h1>ACM ICPC AUT 18th</h1>
                 <p>Dear {{.Name}} of the team {{.Team}}</p>
-                <p>Please write down your login information:</p>
-                <p><strong>Team {{.Team}}</strong></p>
-                <p><strong>Your username: {{.Account.Username}}</strong></p>
-                <p><strong>Your Password: {{.Account.Password}}</strong></p>
-
                 <p>
-                        Wishing ever more success
-                        Team of the 18th International AUT ACM ICPC
-                </p>
+		Today's contest is scheduled to start at 2:30 p.m. Your team's username, password,
+		and any further information will be sent to you 30 minutes before the contest.
+		</p>
+
+		<p>
+		The contest is available <a href="http://daavar.ceit-ssc.ir/domjudge/login">here</a>.
+		</p>
         </body>
 </html>
 `
