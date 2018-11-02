2bdb61a64db40ccdcb0f665b2f6be230d6926fae
Parham Alvani <parham.alvani@gmail.com>
2018-11-02 18:43:33 +0330

    Adds hello template with login template.

--- mail/tmpl.go
+++ mail/tmpl.go
@@ -14,10 +14,29 @@
 package mail
 
 // before contest email. this mail contains just general information about online contest.
+/*
 var tmplString = `
 <html>
         <body>
                 <h1>ACM ICPC AUT 18th</h1>
+                <p>Dear {{.Name}} of the team {{.Team}}</p>
+                <p>
+		Today's contest is scheduled to start at 2:30 p.m. Your team's username, password,
+		and any further information will be sent to you 30 minutes before the contest.
+		</p>
+		<p>
+		The contest is available <a href="http://daavar.ceit-ssc.ir/domjudge/login">here</a>.
+		</p>
+        </body>
+</html>
+`
+*/
+
+// on contest mail. this mail contains the login information.
+var tmplString = `
+<html>
+        <body>
+		<h1>ACM ICPC AUT 18th</h1>
                 <p>Dear {{.Name}} of the team {{.Team}}</p>
 		<p>
 		The contest is available <a href="http://daavar.ceit-ssc.ir/domjudge/login">here</a>.
