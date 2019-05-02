33d444ad359098119155b377fc53c4fb909bdacc
Parham Alvani <parham.alvani@gmail.com>
2019-05-02 20:07:58 +0430

    Brand new mailer without global variables.

--- mail/mail.go
+++ mail/mail.go
@@ -18,25 +18,42 @@ import (
 	gomail "github.com/go-mail/mail"
 )
 
+// mailInfo is a collection of information provided for mail template
 type mailInfo struct {
 	Name    string
 	Team    string
 	Account domjudge.Account
 }
 
-var tmpl *template.Template
+// Mailer sends email using gmail mail server.
+type Mailer struct {
+	Addr    string
+	Pass    string
+	Subject string
 
-func init() {
-	var err error
-	tmpl, err = template.New("mail").Parse(tmplString)
+	tmpl *template.Template
+}
+
+// New creates a new mailer
+func New(addr string, pass string, subject string) (Mailer, error) {
+
+	tmpl, err := template.New("mail").Parse(tmplString)
 	if err != nil {
-		panic(err)
+		return Mailer{}, err
 	}
+
+	return Mailer{
+		Addr:    addr,
+		Pass:    pass,
+		Subject: subject,
+
+		tmpl: tmpl,
+	}, nil
 }
 
 // SendMail sends mail into given mail address with template that is defined in mail.tmpl.
 // This mail contains team and account information and sends per member.
-func SendMail(t domjudge.Team, a domjudge.Account) error {
+func (mailer Mailer) SendMail(t domjudge.Team, a domjudge.Account) error {
 	for _, m := range t.Members {
 
 		if m.Email == "" { // do not send an email to who has no email.
@@ -44,7 +61,7 @@ func SendMail(t domjudge.Team, a domjudge.Account) error {
 		}
 
 		buf := bytes.NewBufferString("")
-		if err := tmpl.Execute(buf, mailInfo{
+		if err := mailer.tmpl.Execute(buf, mailInfo{
 			Name:    m.FirstName,
 			Team:    t.Name,
 			Account: a,
@@ -53,13 +70,13 @@ func SendMail(t domjudge.Team, a domjudge.Account) error {
 		}
 
 		ms := gomail.NewMessage()
-		ms.SetHeader("From", "ceit.ssc94@gmail.com")
+		ms.SetHeader("From", mailer.Addr)
 		ms.SetHeader("To", m.Email)
-		ms.SetAddressHeader("Bcc", "parham.alvani@gmail.com", "1995parham")
-		ms.SetHeader("Subject", "18th AUT ACM ICPC")
+		ms.SetAddressHeader("Bcc", "parham.alvani@gmail.com", "Parham Alvani")
+		ms.SetHeader("Subject", mailer.Subject)
 		ms.SetBody("text/html", buf.String())
 
-		d := gomail.NewDialer("smtp.gmail.com", 465, "ceit.ssc94@gmail.com", "anjomananjoman")
+		d := gomail.NewDialer("smtp.gmail.com", 465, mailer.Addr, mailer.Pass)
 		d.StartTLSPolicy = gomail.MandatoryStartTLS
 
 		if err := d.DialAndSend(ms); err != nil {
