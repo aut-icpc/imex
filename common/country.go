1bff3276e8f19602ca33fd1228d3102399110053
Parham Alvani <parham.alvani@gmail.com>
2019-05-02 19:36:52 +0430

    Uses function instead of map.

--- common/country.go
+++ common/country.go
@@ -12,9 +12,11 @@ package common
 
 // CountryCodes contains a map between 3 character code of countries around the world and their name.
 // AUT-ICPC website works with country name so we must have a way to convert them.
-var CountryCodes = map[string]string{
-	"Iran":                     "IRN",
-	"Germany":                  "DEU",
-	"Sweden":                   "SWE",
-	"United States of America": "USA",
+func CountryCodes(country string) string {
+	return map[string]string{
+		"Iran":                     "IRN",
+		"Germany":                  "DEU",
+		"Sweden":                   "SWE",
+		"United States of America": "USA",
+	}[country]
 }
