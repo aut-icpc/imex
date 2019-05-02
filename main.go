51cb052fe5b875bc5163d69e29b112040573d5a4
Parham Alvani <parham.alvani@gmail.com>
2019-05-02 11:02:17 +0430

    Improves main to parse evand report.

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/AUT-CEIT-SSC/ICPC-imex/aut"
 	"github.com/AUT-CEIT-SSC/ICPC-imex/convert"
 	"github.com/AUT-CEIT-SSC/ICPC-imex/domjudge"
+	"github.com/AUT-CEIT-SSC/ICPC-imex/evand"
 	"github.com/AUT-CEIT-SSC/ICPC-imex/mail"
 	log "github.com/sirupsen/logrus"
 )
@@ -25,15 +26,29 @@ import (
 func main() {
 	var path string
 	var isSendMail bool
+	var tp string
 
-	flag.StringVar(&path, "path", "./data/test.json", "path to the team export of AUT-ICPC website")
+	flag.StringVar(&path, "path", "./data/test.json, ./data/apl.xslx", "path to the team export of AUT-ICPC website or Evand excel")
 	flag.BoolVar(&isSendMail, "sendmail", false, "send mail to online contestant")
+	flag.StringVar(&tp, "type", "aut", "aut, evand")
 
 	flag.Parse()
 
-	onsite, online, err := aut.Import(path)
+	switch tp {
+	case "aut":
+		AUT(path, isSendMail)
+	case "evand":
+		Evand(path)
+	default:
+		fmt.Println()
+	}
+}
+
+// Evand parse Evand excel report and creates the required file.
+func Evand(path string) {
+	onsite, err := evand.Import(path)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// Onsite Teams
@@ -43,42 +58,97 @@ func main() {
 	// domjudge team descriptions
 	fts, err := os.Create("teams-onsite.tsv")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	// domjudge accounts
 	fas, err := os.Create("accounts-onsite.tsv")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	// teams username and passwords for printing.
 	// these are printed for each team and putted in their boxes.
 	fus, err := os.Create("userpass-onsite.csv")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	if _, err := fts.WriteString("teams\t1\n"); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := fas.WriteString("accounts\t1\n"); err != nil {
+		log.Fatal(err)
+	}
+	for i, r := range onsite {
+		t := convert.Convert1(i+400, r)
+		log.Infof("On-Site Team: %+v\n", t)
+
+		if _, err := fts.WriteString(fmt.Sprintf("%d\t%d\t%d\t%s\t%s\t%s\t%s\n", t.Number, t.ExternalID, t.GroupID, t.Name, t.Institution, t.InstitutionCode, t.CountryCode)); err != nil {
+			log.Fatal(err)
+		}
+
+		a := domjudge.NewOnsiteAccount(t)
+
+		log.Infof("On-Site Account: %+v\n", a)
+
+		if _, err := fas.WriteString(fmt.Sprintf("%s\t%s\t%s\t%s\n", a.Type, a.FullName, a.Username, a.Password)); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := fus.WriteString(fmt.Sprintf("%s, %s, %s\n", a.FullName, a.Username, a.Password)); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+// AUT parses AUT-ICPC website JSON report and creates required files.
+func AUT(path string, isSendMail bool) {
+	onsite, online, err := aut.Import(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Onsite Teams
+	// generates password and account number for each team.
+	// final results are written to files.
+
+	// domjudge team descriptions
+	fts, err := os.Create("teams-onsite.tsv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// domjudge accounts
+	fas, err := os.Create("accounts-onsite.tsv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// teams username and passwords for printing.
+	// these are printed for each team and putted in their boxes.
+	fus, err := os.Create("userpass-onsite.csv")
+	if err != nil {
+		log.Fatal(err)
 	}
 	// teams description for reception guys. These guys check these information when teams arrive.
 	frs, err := os.Create("reception-onsite.csv")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	if _, err := fts.WriteString("teams\t1\n"); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	if _, err := fas.WriteString("accounts\t1\n"); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	for i, r := range onsite {
 		t := convert.Convert(i+300, r)
 		log.Infof("On-Site Team: %+v\n", t)
 
 		if _, err := fts.WriteString(fmt.Sprintf("%d\t%d\t%d\t%s\t%s\t%s\t%s\n", t.Number, t.ExternalID, t.GroupID, t.Name, t.Institution, t.InstitutionCode, t.CountryCode)); err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		for _, m := range t.Members {
 			if _, err := frs.WriteString(fmt.Sprintf("%s, %s, %s, %s, %s\n", t.Name, m.FirstName, m.LastName, m.TShirt, m.StudentID)); err != nil {
-				panic(err)
+				log.Fatal(err)
 			}
 		}
 
@@ -87,13 +157,12 @@ func main() {
 		log.Infof("On-Site Account: %+v\n", a)
 
 		if _, err := fas.WriteString(fmt.Sprintf("%s\t%s\t%s\t%s\n", a.Type, a.FullName, a.Username, a.Password)); err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 		if _, err := fus.WriteString(fmt.Sprintf("%s, %s, %s\n", a.FullName, a.Username, a.Password)); err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 	}
-
 	// Online Teams
 	// generates password and account number for each team.
 	// final results are written to files and sent to their emails.
@@ -101,26 +170,26 @@ func main() {
 	// domjudge team descriptions
 	fto, err := os.Create("teams-online.tsv")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	// domjudge accounts
 	fao, err := os.Create("accounts-online.tsv")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	if _, err := fto.WriteString("teams\t1\n"); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	if _, err := fao.WriteString("accounts\t1\n"); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	for i, r := range online {
 		t := convert.Convert(i+300, r)
 		log.Infof("On-Line Team: %+v\n", t)
 
 		if _, err := fto.WriteString(fmt.Sprintf("%d\t%d\t%d\t%s\t%s\t%s\t%s\n", t.Number, t.ExternalID, t.GroupID, t.Name, t.Institution, t.InstitutionCode, t.CountryCode)); err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		a := domjudge.NewOnlineAccount(t)
@@ -128,7 +197,7 @@ func main() {
 		log.Infof("On-Line Account: %+v\n", a)
 
 		if _, err := fao.WriteString(fmt.Sprintf("%s\t%s\t%s\t%s\n", a.Type, a.FullName, a.Username, a.Password)); err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 
 		if isSendMail {
