db6c8b3e27c70795fed4609c6e6f9f4bcdc5675e
Parham Alvani <parham.alvani@gmail.com>
2019-05-02 11:49:02 +0430

    Adds specific conversion function for evand and aut.

--- convert/convert.go
+++ convert/convert.go
@@ -17,13 +17,15 @@ import (
 	"github.com/AUT-CEIT-SSC/ICPC-imex/aut"
 	"github.com/AUT-CEIT-SSC/ICPC-imex/common"
 	"github.com/AUT-CEIT-SSC/ICPC-imex/domjudge"
+	"github.com/AUT-CEIT-SSC/ICPC-imex/evand"
 )
 
-// Convert converts team registration information from AUT-ICPC to domjudge team.
 // please note that domjudge need an unique identifier for each team. If this identifier
 // is not unique then old record in domjudge will override.
 // ExternalID is not important and you can even do not pass it to Domjudge.
-func Convert(id int, r aut.Register) domjudge.Team {
+
+// FromAUT converts team registration information from AUT-ICPC to domjudge team.
+func FromAUT(id int, r aut.Register) domjudge.Team {
 	return domjudge.Team{
 		Number:          id,
 		ExternalID:      id,
@@ -36,6 +38,23 @@ func Convert(id int, r aut.Register) domjudge.Team {
 			r.Members.First,
 			r.Members.Second,
 			r.Members.Third,
+		},
+	}
+}
+
+// FromEvand converts team registration information from AUT-ICPC to domjudge team.
+func FromEvand(id int, r evand.Register) domjudge.Team {
+	return domjudge.Team{
+		Number:          id,
+		ExternalID:      id,
+		GroupID:         3,
+		Name:            r.Name,
+		Institution:     r.Institute,
+		InstitutionCode: common.InstitutionCodes[r.Institute],
+		CountryCode:     common.CountryCodes["Iran"],
+		Members: [3]common.Member{
+			r.Members.First,
+			r.Members.Second,
 		},
 	}
 }
